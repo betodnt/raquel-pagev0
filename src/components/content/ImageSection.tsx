413ad295a014038c00b8d921a0c4fc0bb9bd011b
betodnt <dentgm@gmail.com>
2024-08-13 16:38:14 -0300

    Corrigir erros de formatação com Prettier

--- src/components/content/ImageSection.tsx
+++ src/components/content/ImageSection.tsx
@@ -1,22 +1,23 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 function ImageSection() {
   return (
-    <div className='flex flex-col items-center md:flex-row md:justify-between'>
+    <div className="flex flex-col items-center md:flex-row md:justify-between">
       <Image
-        src='/images/profile.png'
-        alt='Raquel Rodrigues Masson'
+        src="/images/profile.png"
+        alt="Raquel Rodrigues Masson"
         width={1000}
         height={1000}
-        className='rounded-full my-7 w-32 h-32 md:mr-10 md:mb-0'
+        className="rounded-full my-7 w-32 h-32 md:mr-10 md:mb-0"
       />
-      <p className='text-center text-2xl md:text-left md:mt-11 md:text-xl'>
+      <p className="text-center text-2xl md:text-left md:mt-11 md:text-xl">
         Olá! Seja bem-vindo(a) <br />
-        Me chamo Raquel Rodrigues Masson, uma recém-formada em direito apaixonada por justiça e
-        comprometida em ajudar você a garantir seus direitos.
+        Me chamo Raquel Rodrigues Masson, uma recém-formada em direito
+        apaixonada por justiça e comprometida em ajudar você a garantir seus
+        direitos.
       </p>
     </div>
-  );
+  )
 }
 
-export default ImageSection;
+export default ImageSection
