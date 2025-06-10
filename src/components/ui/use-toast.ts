41cc4d74c1f90d61178f3aebcadee00a8be72f03
Roberto Sabino <110215312+betodnt@users.noreply.github.com>
2025-06-10 20:00:55 -0300

    Revert "Feat/modernize project"

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -15,6 +15,13 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+const actionTypes = {
+  ADD_TOAST: 'ADD_TOAST',
+  UPDATE_TOAST: 'UPDATE_TOAST',
+  DISMISS_TOAST: 'DISMISS_TOAST',
+  REMOVE_TOAST: 'REMOVE_TOAST',
+} as const
+
 let count = 0
 
 function genId() {
@@ -22,21 +29,23 @@ function genId() {
   return count.toString()
 }
 
+type ActionType = typeof actionTypes
+
 type Action =
   | {
-      type: 'ADD_TOAST'
+      type: ActionType['ADD_TOAST']
       toast: ToasterToast
     }
   | {
-      type: 'UPDATE_TOAST'
+      type: ActionType['UPDATE_TOAST']
       toast: Partial<ToasterToast>
     }
   | {
-      type: 'DISMISS_TOAST'
+      type: ActionType['DISMISS_TOAST']
       toastId?: ToasterToast['id']
     }
   | {
-      type: 'REMOVE_TOAST'
+      type: ActionType['REMOVE_TOAST']
       toastId?: ToasterToast['id']
     }
 
@@ -55,7 +64,7 @@ const addToRemoveQueue = (toastId: string) => {
     toastTimeouts.delete(toastId)
     dispatch({
       type: 'REMOVE_TOAST',
-      toastId, // Corrected
+      toastId: toastId,
     })
   }, TOAST_REMOVE_DELAY)
 
@@ -153,7 +162,7 @@ function toast({ ...props }: Toast) {
   })
 
   return {
-    id, // Corrected
+    id: id,
     dismiss,
     update,
   }
